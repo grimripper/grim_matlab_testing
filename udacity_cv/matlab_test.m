14ab17bc0b0ae4d056b37dd441b715a03d2d8ba2
Grim Ripper <ripper.grim@gmail.com>
2016-09-03 16:53:14 -0700

    delete old

--- udacity_cv/matlab_test.m
+++
@@ -1,5 +0,0 @@
-%http://www.cc.gatech.edu/~afb/classes/CS4495-Fall2014/toolresources/matlabtutorial1.pdf
-
-%"dbstop if error" before running a script for debugging
-
-
--- udacity_cv/test003_colorplanes.m
+++
@@ -1,8 +0,0 @@
-% Color planes
-im = imread('lena.png');
-disp(size(im))
-
-img_red = im(:,:,1);
-imshow(img_red)
-
-plot(img_red(150, :))
